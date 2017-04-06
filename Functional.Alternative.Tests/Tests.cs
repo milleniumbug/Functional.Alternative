8efc17364132712aa4257f27d3402e2a7fdba1ad
milleniumbug <wojas1222@gmail.com>
2017-04-06 20:57:51 +0200

    override ToString and equality

--- Functional.Alternative.Tests/Tests.cs
+++ Functional.Alternative.Tests/Tests.cs
@@ -35,6 +35,28 @@ namespace Functional.Alternative.Tests
         }
 
         [Test]
+        public void ToStringTest()
+        {
+            var variant = new Alternative<int, string>(42);
+            Assert.AreEqual(variant.ToString(), "(System.Int32 42)");
+            variant = new Alternative<int, string>("John Rambo");
+            Assert.AreEqual(variant.ToString(), "(System.String John Rambo)");
+        }
+
+        [Test]
+        public void EqualityTest()
+        {
+            var variant = new Alternative<int, string>(42);
+            Assert.AreEqual(variant, variant);
+            var variantSame = new Alternative<int, string>(42);
+            Assert.AreEqual(variant, variantSame);
+            Assert.AreEqual(variantSame, variant);
+            var variantOther = new Alternative<int, string>("John Rambo");
+            Assert.AreNotEqual(variant, variantOther);
+            Assert.AreNotEqual(variantOther, variant);
+        }
+
+        [Test]
         public void ThrowOnInvalidAccess()
         {
             var variant = new Alternative<int, string>("John Rambo");
